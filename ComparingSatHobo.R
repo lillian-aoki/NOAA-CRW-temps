134de6fe2c69234d0d5146dcf996061b9c41c24b
Lillian Aoki <lra53@cornell.edu>
2020-01-25 23:05:38 -0800

    Plots of disease vs temperature

--- ComparingSatHobo.R
+++ ComparingSatHobo.R
@@ -9,8 +9,10 @@ sat$time <- as.POSIXct(sat$time,tz="")
 
 all_summ <- read.csv("~/Box/projects/NSF WD/HOBOs/WD-temperature/Allsites.summJuly.2019.csv")
 all_summ$Day <- as.POSIXct(all_summ$Day,tz="")
-
-combo <- inner_join(sat,all_summ,by=c("Region","SiteCode","time"="Day"))
+all_summ_ave <- all_summ %>%
+  group_by(Region,SiteCode,Day) %>%
+  summarise(DailyMean=mean(DailyMean))
+combo <- inner_join(sat,all_summ_ave,by=c("Region","SiteCode","time"="Day"))
 
 # for(i in length(unique(combo$SiteCode))){
 #   model <- lm(DailyMean~CRW_SST,data=subset(combo,SiteCode==i))
@@ -27,7 +29,8 @@ for(i in levels(combo$Region.SiteCode)){
   model <- lm(DailyMean~CRW_SST,data=combo[combo$Region.SiteCode==i,])
   sig_table_temp <- data.frame("Region.SiteCode"=i,
                                "Pvalue"=as.character(signif(summary(model)$coef[2,4], 2)),
-                               "AdjR2"=as.character(signif(summary(model)$adj.r.squared, 2)))
+                               "AdjR2"=as.character(signif(summary(model)$adj.r.squared, 2)),
+                               "Slope"=as.character(signif(summary(model)$coef[2,1],2)))
   sig_table_temp <- separate(sig_table_temp,col=Region.SiteCode,into=c("Region","SiteCode"),sep=" ")
   if(first_write==1){
     sig_table <- sig_table_temp
@@ -36,6 +39,11 @@ for(i in levels(combo$Region.SiteCode)){
     sig_table <- rbind(sig_table,sig_table_temp)
   }
 }
+
+sig_table_numeric <- sig_table
+sig_table_numeric$Pvalue <- as.numeric(as.character(sig_table_numeric$Pvalue))
+sig_table_numeric$AdjR2 <- as.numeric(as.character(sig_table_numeric$AdjR2))
+sig_table_numeric$Slope <- as.numeric(as.character(sig_table_numeric$Slope))
 
 ggplot(data=combo[combo$Region=="AK",],aes(x=CRW_SST,y=DailyMean))+geom_point()+
   stat_smooth(method="lm",col="dark grey")+
@@ -53,8 +61,8 @@ ggplot(data=combo[combo$Region=="AK",],aes(x=CRW_SST,y=DailyMean))+geom_point()+
 ggplot(data=combo[combo$Region=="BC",],aes(x=CRW_SST,y=DailyMean))+geom_point()+
   stat_smooth(method="lm",col="dark grey")+
   geom_abline(slope=1,intercept=0,linetype="dashed")+
-  geom_text(data=sig_table[sig_table$Region=="BC",],aes(x=13,y=18,label=paste("p =",Pvalue)))+
-  geom_text(data=sig_table[sig_table$Region=="BC",],aes(x=13,y=18.5,label=paste("Adj R2 =",AdjR2)))+
+  geom_text(data=sig_table[sig_table$Region=="BC",],aes(x=14.75,y=16,label=paste("p =",Pvalue)))+
+  geom_text(data=sig_table[sig_table$Region=="BC",],aes(x=14.75,y=16.5,label=paste("Adj R2 =",AdjR2)))+
   facet_wrap(~SiteCode)+
   xlab("Nighttime SST via satellite (ºC)")+
   ylab("Daily mean temp via HOBO logger (ºC)")+
@@ -99,6 +107,19 @@ ggplot(data=combo[combo$Region=="BB",],aes(x=CRW_SST,y=DailyMean))+geom_point()+
   xlab("Nighttime SST via satellite (ºC)")+
   ylab("Daily mean temp via HOBO logger (ºC)")+
   labs(title="Comparing satellite and in situ temperatures - Bodega Bay",
+       subtitle = "July 2019")+
+  theme_bw()+
+  theme(strip.background = element_rect(fill="white"))
+
+ggplot(data=combo[combo$Region=="SD",],aes(x=CRW_SST,y=DailyMean))+geom_point()+
+  stat_smooth(method="lm",col="dark grey")+
+  geom_abline(slope=1,intercept=0,linetype="dashed")+
+  geom_text(data=sig_table[sig_table$Region=="SD",],aes(x=19,y=26.5,label=paste("p =",Pvalue)))+
+  geom_text(data=sig_table[sig_table$Region=="SD",],aes(x=19,y=27,label=paste("Adj R2 =",AdjR2)))+
+  facet_wrap(~SiteCode)+
+  xlab("Nighttime SST via satellite (ºC)")+
+  ylab("Daily mean temp via HOBO logger (ºC)")+
+  labs(title="Comparing satellite and in situ temperatures - San Diego",
        subtitle = "July 2019")+
   theme_bw()+
   theme(strip.background = element_rect(fill="white"))
---
+++ DiseaseVsTemp.md
@@ -0,0 +1,14 @@
+DiseaseTemp
+================
+LRA
+1/25/2020
+
+### Plots of wasting disease metrics against mid-summer temperature
+
+### In situ temperatures
+
+![](DiseaseVsTemp_files/figure-gfm/disease_in_situ-1.png)<!-- -->![](DiseaseVsTemp_files/figure-gfm/disease_in_situ-2.png)<!-- -->
+
+### Satellite temperatures
+
+![](DiseaseVsTemp_files/figure-gfm/disease_sat-1.png)<!-- -->![](DiseaseVsTemp_files/figure-gfm/disease_sat-2.png)<!-- -->![](DiseaseVsTemp_files/figure-gfm/disease_sat-3.png)<!-- -->![](DiseaseVsTemp_files/figure-gfm/disease_sat-4.png)<!-- -->
